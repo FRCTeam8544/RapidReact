1e6847631dfa7ac144e05139f75c58b0e30309d8
jaja18264 <70504636+jaja18264@users.noreply.github.com>
2022-02-05 15:24:45 -0500

    Add files via upload

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,11 +7,15 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.TankDrive;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.ArcadeDrive;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -28,18 +32,19 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final DriveTrain m_drivetrain = new DriveTrain();
+  
 
   //robot commands 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   private final TankDrive m_tankDrive = new TankDrive(m_drivetrain);
-
+  private final ArcadeDrive m_arcadeDrive = new ArcadeDrive(m_drivetrain);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
 
     //defining default commands
-    m_drivetrain.setDefaultCommand(m_tankDrive);
+    m_drivetrain.setDefaultCommand(m_arcadeDrive);
   }
 
   /**
@@ -48,8 +53,23 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    JoystickButton button1 = new JoystickButton(joystick1, Constants.ROBOTCONTAINER_JOYSTICK_BASE_MODESWITCH_ID);
+    JoystickButton button2 = new JoystickButton(joystick2, Constants.ROBOTCONTAINER_JOYSTICK_BASE_MODESWITCH_ID);
+    button1.and(button2).whenActive(() -> this.SwitchDriveMode());
 
+  }
+
+
+  public void SwitchDriveMode() {
+    if (m_arcadeDrive.getDriveType().equals("tank")) {
+        m_arcadeDrive.setDriveType("arcade");
+        SmartDashboard.putString("DriveType", "arcade");
+      } else {
+        m_arcadeDrive.setDriveType("tank");
+        SmartDashboard.putString("DriveType", "arcade");
+      }
+  }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
